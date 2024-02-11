f036e2d6577432b73e0cffa50c0767af66500a4d
Sai Sathish <sai.sathish@modivcare.com>
2024-02-11 14:09:44 +0530

    first commit

---
+++ components/Button.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+type ButtonProps = {
+  type: "button" | "submit";
+  title: string;
+  icon?: string;
+  variant:
+    | "btn_dark_green"
+    | "btn_green"
+    | "btn_white_text"
+    | "btn_white"
+    | "btn_dark_green_outline";
+  fullWidth?: boolean;
+};
+
+const Button = ({ type, title, icon, variant, fullWidth }: ButtonProps) => {
+  return (
+    <button
+      className={`flexCenter gap-3 rounded-full border ${variant} ${
+        fullWidth && "w-full"
+      }`}
+      type={type}
+    >
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <label className="bold-16 whitespace-nowrap cursor-pointer">
+        {title}
+      </label>
+    </button>
+  );
+};
+
+export default Button;
