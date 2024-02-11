c02490a93457fe190f49c9180d3fcedf56b5a401
Saisathish <Sai.m.r.sathish@gmail.com>
2024-02-11 19:30:23 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+  basePath: isProd ? "/Travel-website" : "",
   output: "export",
   images: { unoptimized: true },
 };
